c2e2589fc11644c816109f038433be4d91f1c4dc
gabrielefrancescutto <gabriele@athand.nl>
2023-04-24 15:36:19 +0200

    fix

--- ecommerce-api/modules/Stripe/Listeners/StripeEventListener.php
+++ ecommerce-api/modules/Stripe/Listeners/StripeEventListener.php
@@ -62,7 +62,7 @@ class StripeEventListener
                 $processorShipping = resolve(GetShippingCostProcessor::class);
                 $processorAmount = resolve(GetSubAndTotalAmountForOrderProcessor::class);
 
-                $lastOrderNumber = Order::orderBy('order_number')->first();    
+                $lastOrderNumber = Order::whereNotNull('order_number')->orderBy('order_number')->first();    
                 if($lastOrderNumber === null) {
                     $orderNumber = 1;
                 } else {
