57500d61951cf4d8f6f3352285d2dc7d9758811d
gabrielefrancescutto <gabriele@athand.nl>
2023-04-24 16:06:16 +0200

    fix

--- ecommerce-api/public/web/src_app_home_home_module_ts.js
+++ ecommerce-api/public/web/src_app_home_home_module_ts.js
@@ -632,7 +632,7 @@ function FashionTwoComponent_ng_container_22_Template(rf, ctx) {
 }
 const _c1 = function () {
   return {
-    "background-image": "url(web/assets/images/homepage/lower.jpg)"
+    "background-image": "url(web/assets/images/homepage/lower.webp)"
   };
 };
 class FashionTwoComponent {
@@ -645,26 +645,26 @@ class FashionTwoComponent {
     this.newProductCollection = [];
     this.HomeSliderConfig = _shared_data_slider__WEBPACK_IMPORTED_MODULE_0__.HomeSlider;
     this.sliders = [{
-      image: 'web/assets/images/homepage/slider-1.jpg',
+      image: 'web/assets/images/homepage/slider-1.webp',
       url: '/shop/product/7'
     }, {
-      image: 'web/assets/images/homepage/slider-2.jpg',
+      image: 'web/assets/images/homepage/slider-2.webp',
       url: '/shop/product/6'
     }, {
-      image: 'web/assets/images/homepage/slider-3.jpg',
+      image: 'web/assets/images/homepage/slider-3.webp',
       url: '/shop/product/4'
     }, {
-      image: 'web/assets/images/homepage/slider-4.jpg',
+      image: 'web/assets/images/homepage/slider-4.webp',
       url: '/shop/product/14'
     }, {
-      image: 'web/assets/images/homepage/slider-5.jpg',
+      image: 'web/assets/images/homepage/slider-5.webp',
       url: '/shop/product/15'
     }];
     this.grid = 'col-6';
     this.layoutView = 'grid-view';
     // Collection banner
     this.collections1 = [{
-      image: 'web/assets/images/homepage/category/mtb.jpg',
+      image: 'web/assets/images/homepage/category/mtb.webp',
       save: '',
       title: 'MTB',
       url: '/shop',
@@ -676,7 +676,7 @@ class FashionTwoComponent {
         left: '43%'
       }
     }, {
-      image: 'web/assets/images/homepage/category/streetwear.jpg',
+      image: 'web/assets/images/homepage/category/streetwear.webp',
       save: '',
       title: 'STREETWEAR',
       url: '/shop',
@@ -711,7 +711,7 @@ FashionTwoComponent.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_
   selectors: [["app-fashion-two"]],
   decls: 34,
   vars: 10,
-  consts: [[3, "sticky"], [1, "p-0"], [3, "sliders", "textClass", "category", "buttonText"], [1, "row"], [1, "col-12", "text-center", "home-spedizioni", 2, "margin-top", "2%", "color", "#0fb3db"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 -117 679.99892 679", 1, "svg"], ["d", "m12.347656 378.382812h37.390625c4.371094 37.714844 36.316407 66.164063 74.277344 66.164063 37.96875 0 69.90625-28.449219 74.28125-66.164063h241.789063c4.382812 37.714844 36.316406 66.164063 74.277343 66.164063 37.96875 0 69.902344-28.449219 74.285157-66.164063h78.890624c6.882813 0 12.460938-5.578124 12.460938-12.460937v-352.957031c0-6.882813-5.578125-12.464844-12.460938-12.464844h-432.476562c-6.875 0-12.457031 5.582031-12.457031 12.464844v69.914062h-105.570313c-4.074218.011719-7.890625 2.007813-10.21875 5.363282l-68.171875 97.582031-26.667969 37.390625-9.722656 13.835937c-1.457031 2.082031-2.2421872 4.558594-2.24999975 7.101563v121.398437c-.09765625 3.34375 1.15624975 6.589844 3.47656275 9.003907 2.320312 2.417968 5.519531 3.796874 8.867187 3.828124zm111.417969 37.386719c-27.527344 0-49.851563-22.320312-49.851563-49.847656 0-27.535156 22.324219-49.855469 49.851563-49.855469 27.535156 0 49.855469 22.320313 49.855469 49.855469 0 27.632813-22.21875 50.132813-49.855469 50.472656zm390.347656 0c-27.53125 0-49.855469-22.320312-49.855469-49.847656 0-27.535156 22.324219-49.855469 49.855469-49.855469 27.539063 0 49.855469 22.320313 49.855469 49.855469.003906 27.632813-22.21875 50.132813-49.855469 50.472656zm140.710938-390.34375v223.34375h-338.375c-6.882813 0-12.464844 5.578125-12.464844 12.460938 0 6.882812 5.582031 12.464843 12.464844 12.464843h338.375v79.761719h-66.421875c-4.382813-37.710937-36.320313-66.15625-74.289063-66.15625-37.960937 0-69.898437 28.445313-74.277343 66.15625h-192.308594v-271.324219h89.980468c6.882813 0 12.464844-5.582031 12.464844-12.464843 0-6.882813-5.582031-12.464844-12.464844-12.464844h-89.980468v-31.777344zm-531.304688 82.382813h99.703125v245.648437h-24.925781c-4.375-37.710937-36.3125-66.15625-74.28125-66.15625-37.960937 0-69.90625 28.445313-74.277344 66.15625h-24.929687v-105.316406l3.738281-5.359375h152.054687c6.882813 0 12.460938-5.574219 12.460938-12.457031v-92.226563c0-6.882812-5.578125-12.464844-12.460938-12.464844h-69.796874zm-30.160156 43h74.777344v67.296875h-122.265625zm0 0", "fill", "#ff4c3b"], [1, "banner-padding", "banner-furniture"], [1, "container-fluid"], [1, "row", "partition2"], ["class", "col-md-6", 4, "ngFor", "ngForOf"], [1, "title1", "section-t-space"], [1, "title-inner1", "font-size-mobile"], [1, "section-b-space", "p-t-0"], [1, "container"], [1, "col", "theme-tab"], [1, "row", "no-slider"], [4, "ngFor", "ngForOf"], [1, "row", "footer-image-only-mobile"], [1, "col"], [1, "col-10"], ["src", "web/assets/images/homepage/lower.jpg", "alt", "footer_image", 2, "width", "100%"], [1, "p-0", "footer-image-only-desktop"], [1, "full-banner", "parallax-banner1", "parallax", "text-center", "p-left", 3, "ngStyle"], [1, "col", 2, "min-height", "150px"], [3, "themeLogo"], [1, "col-md-6"], [3, "routerLink", "queryParams"], [1, "collection-banner", "p-left", "text-start"], ["alt", "collection-banner", 1, "img-fluid", 3, "src"], [1, "contain-banner"], [1, "collection-titles", 3, "ngStyle"], [1, "col-6", "product-box"], [3, "product", "currency"]],
+  consts: [[3, "sticky"], [1, "p-0"], [3, "sliders", "textClass", "category", "buttonText"], [1, "row"], [1, "col-12", "text-center", "home-spedizioni", 2, "margin-top", "2%", "color", "#0fb3db"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 -117 679.99892 679", 1, "svg"], ["d", "m12.347656 378.382812h37.390625c4.371094 37.714844 36.316407 66.164063 74.277344 66.164063 37.96875 0 69.90625-28.449219 74.28125-66.164063h241.789063c4.382812 37.714844 36.316406 66.164063 74.277343 66.164063 37.96875 0 69.902344-28.449219 74.285157-66.164063h78.890624c6.882813 0 12.460938-5.578124 12.460938-12.460937v-352.957031c0-6.882813-5.578125-12.464844-12.460938-12.464844h-432.476562c-6.875 0-12.457031 5.582031-12.457031 12.464844v69.914062h-105.570313c-4.074218.011719-7.890625 2.007813-10.21875 5.363282l-68.171875 97.582031-26.667969 37.390625-9.722656 13.835937c-1.457031 2.082031-2.2421872 4.558594-2.24999975 7.101563v121.398437c-.09765625 3.34375 1.15624975 6.589844 3.47656275 9.003907 2.320312 2.417968 5.519531 3.796874 8.867187 3.828124zm111.417969 37.386719c-27.527344 0-49.851563-22.320312-49.851563-49.847656 0-27.535156 22.324219-49.855469 49.851563-49.855469 27.535156 0 49.855469 22.320313 49.855469 49.855469 0 27.632813-22.21875 50.132813-49.855469 50.472656zm390.347656 0c-27.53125 0-49.855469-22.320312-49.855469-49.847656 0-27.535156 22.324219-49.855469 49.855469-49.855469 27.539063 0 49.855469 22.320313 49.855469 49.855469.003906 27.632813-22.21875 50.132813-49.855469 50.472656zm140.710938-390.34375v223.34375h-338.375c-6.882813 0-12.464844 5.578125-12.464844 12.460938 0 6.882812 5.582031 12.464843 12.464844 12.464843h338.375v79.761719h-66.421875c-4.382813-37.710937-36.320313-66.15625-74.289063-66.15625-37.960937 0-69.898437 28.445313-74.277343 66.15625h-192.308594v-271.324219h89.980468c6.882813 0 12.464844-5.582031 12.464844-12.464843 0-6.882813-5.582031-12.464844-12.464844-12.464844h-89.980468v-31.777344zm-531.304688 82.382813h99.703125v245.648437h-24.925781c-4.375-37.710937-36.3125-66.15625-74.28125-66.15625-37.960937 0-69.90625 28.445313-74.277344 66.15625h-24.929687v-105.316406l3.738281-5.359375h152.054687c6.882813 0 12.460938-5.574219 12.460938-12.457031v-92.226563c0-6.882812-5.578125-12.464844-12.460938-12.464844h-69.796874zm-30.160156 43h74.777344v67.296875h-122.265625zm0 0", "fill", "#ff4c3b"], [1, "banner-padding", "banner-furniture"], [1, "container-fluid"], [1, "row", "partition2"], ["class", "col-md-6", 4, "ngFor", "ngForOf"], [1, "title1", "section-t-space"], [1, "title-inner1", "font-size-mobile"], [1, "section-b-space", "p-t-0"], [1, "container"], [1, "col", "theme-tab"], [1, "row", "no-slider"], [4, "ngFor", "ngForOf"], [1, "row", "footer-image-only-mobile"], [1, "col"], [1, "col-10"], ["src", "web/assets/images/homepage/lower.webp", "alt", "footer_image", 2, "width", "100%"], [1, "p-0", "footer-image-only-desktop"], [1, "full-banner", "parallax-banner1", "parallax", "text-center", "p-left", 3, "ngStyle"], [1, "col", 2, "min-height", "150px"], [3, "themeLogo"], [1, "col-md-6"], [3, "routerLink", "queryParams"], [1, "collection-banner", "p-left", "text-start"], ["alt", "collection-banner", 1, "img-fluid", 3, "src"], [1, "contain-banner"], [1, "collection-titles", 3, "ngStyle"], [1, "col-6", "product-box"], [3, "product", "currency"]],
   template: function FashionTwoComponent_Template(rf, ctx) {
     if (rf & 1) {
       _angular_core__WEBPACK_IMPORTED_MODULE_7__["ɵɵelement"](0, "app-header-one", 0);
