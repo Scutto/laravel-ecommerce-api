724bc76d25bf564573ea7eb3c52a83aa2e6b149a
gabrielefrancescutto <gabriele@athand.nl>
2024-08-05 20:30:43 +0200

    fix descriptions

--- ecommerce-api/resources/views/descriptions/snapback.blade.php
+++ ecommerce-api/resources/views/descriptions/snapback.blade.php
@@ -2,7 +2,7 @@
     <li style="width: 100%;">- Fori al laser posti in quattro delle sei sezioni della corona danno maggiore traspirabilità.</li>
     <li style="width: 100%;">- Chiusura posteriore regolabile con bottone a pressione per garantire una comoda vestibilita</li>
     <li style="width: 100%;">- Ricamo in rilievo frontale con contrasto di colore</li>
-    <li style="width: 100%;">- Tessuto in 100% Polyestere, disponibile in due colorazioni.</li>
+    <li style="width: 100%;">- Tessuto in 100% Polyestere.</li>
 </ul>
 <br/>
 <p>Disponibile in due colorazioni - Unisex fit</p>
